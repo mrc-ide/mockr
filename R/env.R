61ff4551e57e5e22ac95006f8b7b38ccf5f0a91a
Robert Ashton <r.ashton@imperial.ac.uk>
2023-01-05 17:36:05 +0000

    Make mockr work with onLoad

--- R/env.R
+++ R/env.R
@@ -108,7 +108,7 @@ create_mock_env_with_old_funcs <- function(new_funcs, .env, .parent) {
   # retrieve all functions not mocked
   old_funcs <- as.list(.env, all.names = TRUE)
   old_funcs <- old_funcs[vlapply(old_funcs, is.function)]
-  old_funcs <- old_funcs[!(names(old_funcs) %in% names(new_funcs))]
+  old_funcs <- old_funcs[!(names(old_funcs) %in% c(".onLoad", names(new_funcs)))]
 
   # query value visible from .parent to support nesting
   old_funcs <- mget(names(old_funcs), .parent, inherits = TRUE)
